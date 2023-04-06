de2839bfedb08fc70b51c42afc7d10daf904ade0
suriikata <iris.cilensek1@gmail.com>
2023-04-06 19:34:53 +0200

    sivka

---
+++ PDF.py
@@ -0,0 +1,93 @@
+import random
+from os import listdir
+from pypdf import PdfReader
+from re import finditer
+from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+import requests
+
+def find_all_indexes(word, text):
+    indexes = []
+    for match in finditer(word, text):
+        indexes.append(match.start())
+
+    return indexes
+
+def get_abstract(text):
+    start_indexes = find_all_indexes('POVZETEK', text)
+    end_indexes = find_all_indexes('ABSTRACT', text)
+
+    abstracts = []
+    for start_index, end_index in zip(start_indexes, end_indexes):
+        abstract_start = start_index + len('POVZETEK')
+        abstract_end = end_index
+        abstract = text[abstract_start:abstract_end].rstrip()
+        if 'poplav' in abstract:
+            abstracts.append(abstract)
+    """
+    for a in abstracts:
+        print(a)
+    """
+    return abstracts
+
+def create_txt(abstracts, prefix):
+    rand = random.randint(0, 10000)
+    for a in abstracts:
+        filename = f'output/{prefix}_{rand}.txt'
+        print(f'\tsaving abstract to {filename}')
+        with open(filename, 'wb') as f:
+            f.write(a.encode('utf-8'))
+
+
+def get_inputs():
+    paths = []
+    pdfs = listdir('slovenski_vodar')
+    for pdf in pdfs:
+        paths.append(f'slovenski_vodar/{pdf}')
+    return paths
+
+def get_driver():
+    driver = Firefox(service=Service(executable_path=GeckoDriverManager().install()))
+    driver.implicitly_wait(3)
+    return driver
+
+def download(url, local_path):
+    response = requests.get(url)
+    with open(local_path, 'wb') as f:
+        f.write(response.content)
+
+
+def scrape():
+    sv_url = "http://www.drustvo-vodarjev.si/slovenski-vodar/"
+    driver = get_driver()
+    driver.get(sv_url)
+    down_links = driver.find_elements(By.CLASS_NAME, 'filename')
+    n = len(down_links)
+    for i in range(n):
+        down_links = driver.find_elements(By.CLASS_NAME, 'filename')
+        down_link = down_links[i]
+        url = down_link.find_element(By.TAG_NAME, 'a')
+        url = url.get_property('href')
+        filename = url.split('/')[-1]
+        print(f'i={i} Trying to save {url} to filename {filename}')
+        download(url, f'slovenski_vodar/{filename}')
+
+#scrape()
+
+if __name__ == '__main__':
+    journals = get_inputs()
+
+    for journal in journals:
+        reader = PdfReader(journal)
+        text = ""
+
+        for page in reader.pages:
+            text += page.extract_text() + "\n"
+        print(f'extracting abstracts from {journal}')
+        bedarija = get_abstract(text)
+        create_txt(bedarija, "SV")
+
+
+
--- dLib.py
+++ dLib.py
@@ -3,21 +3,21 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from bs4 import BeautifulSoup
+from PDF import create_txt
 import requests
 
 
-
 def find_description(url):
-    """ finds description on current page. returns url with description. """
     text = BeautifulSoup(requests.get(url).content, features = 'lxml')
     abstract = ""
     try:
         div_descript = text.find('div', text = 'Opis')
         abstract = div_descript.findNext().text
+        create_txt([abstract], 'dLib')
+        print(abstract)
     except Exception as e:
         print(f'tezave pri iskanju opisa za {url}')
 
-    print(abstract)
     return url, abstract
 
 def get_driver():
@@ -25,18 +25,23 @@ def get_driver():
     driver.implicitly_wait(3)
     return driver
 
-def scrape(i):
-    # Acta Hydrotechnica
-    dlib_url = 'https://www.dlib.si/results/?euapi=1&query=%27keywords%3dpoplave*%27&sortDir=ASC&sort=date&pageSize=25&frelation=Acta+hydrotechnica&flanguage=slv'
+def scrape(url):
     driver = get_driver()
-    driver.get(dlib_url)
-    titles = driver.find_elements(By.CLASS_NAME, 'Naslov')
-    title = titles[i]
-    url = title.find_element(By.TAG_NAME, 'a')
-    url = url.get_property('href')
     driver.get(url)
-    return url
+    titles = driver.find_elements(By.CLASS_NAME, 'Naslov')
+    n = len(titles)
+    for i in range(n):
+        titles = driver.find_elements(By.CLASS_NAME, 'Naslov')
+        title = titles[i]
+        url = title.find_element(By.TAG_NAME, 'a')
+        url = url.get_property('href')
+        find_description(url)
 
 
-skropucalo = scrape(3)
-find_description(skropucalo)
+# Gradbeni vestnik (2) & Acta hydrotechnica (7)
+dlib_urls = ['https://www.dlib.si/results/?euapi=1&query=%27keywords%3dpoplave%2c+gradbeni+vestnik*%27&sortDir=ASC&sort=date&pageSize=25',
+            'https://www.dlib.si/results/?euapi=1&query=%27keywords%3dpoplave*%27&sortDir=ASC&sort=date&pageSize=25&frelation=Acta+hydrotechnica&flanguage=slv']
+
+for url in dlib_urls:
+    print(f'\n\nzacenjam z {url} \n\n')
+    scrape(url)
